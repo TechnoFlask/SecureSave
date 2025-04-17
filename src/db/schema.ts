2552276b0e8047fdd916f0278a8e1573d2c9165c
Anish Mukherjee <technoflask03@gmail.com>
2025-04-17 23:39:44 +0530

    fix: restructure schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,31 +1,25 @@
 import { pgTable, uuid, varchar, timestamp, pgEnum } from "drizzle-orm/pg-core"
 
-export const passwordsTable = pgTable("passwords", {
+export const credTypeEnum = pgEnum("credType", ["passwords", "cards"])
+
+export const credsTable = pgTable("creds", {
     id: uuid().primaryKey().defaultRandom(),
     userId: varchar().notNull(),
+    credType: credTypeEnum().notNull(),
     name: varchar().notNull(),
     iv: varchar().notNull(),
     enc: varchar().notNull(),
     salt: varchar().notNull(),
     createdAt: timestamp().notNull().defaultNow(),
 })
-
-export const cardsTable = pgTable("cards", {
-    id: uuid().primaryKey().defaultRandom(),
-    userId: varchar().notNull(),
-    name: varchar().notNull(),
-    iv: varchar().notNull(),
-    enc: varchar().notNull(),
-    salt: varchar().notNull(),
-    createdAt: timestamp().notNull().defaultNow(),
-})
-
-export const credTypeEnum = pgEnum("cred_type", ["passwords", "cards"])
 
 export const shareCredsTable = pgTable("shared_creds", {
     id: uuid().primaryKey().defaultRandom(),
     credType: credTypeEnum().notNull(),
     sender: varchar().notNull(),
+    credId: uuid()
+        .notNull()
+        .references(() => credsTable.id, { onDelete: "cascade" }),
     recipient: varchar().notNull(),
     iv: varchar().notNull(),
     enc: varchar().notNull(),
