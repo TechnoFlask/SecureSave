9b4152990fd67359cac8dbce8f6997ff52fd2feb
Anish Mukherjee <technoflask03@gmail.com>
2025-04-20 17:32:03 +0530

    fix: sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,20 @@
 import { SignIn } from "@clerk/nextjs"
 
 export default function Page() {
-    return <SignIn />
+    return (
+        <div
+            style={{
+                background:
+                    "linear-gradient(135deg, #f1fdfb 0%, #eaf8ff 25%, #edfef4 50%, #f4fbff 75%, #ffffff 100%)",
+            }}
+            className="h-screen fixed inset-0 z-0"
+        >
+            <div
+                style={{ backdropFilter: "blur(10px)" }}
+                className="fixed inset-0 z-10 flex justify-center items-center bg-transparent"
+            >
+                <SignIn />
+            </div>
+        </div>
+    )
 }
